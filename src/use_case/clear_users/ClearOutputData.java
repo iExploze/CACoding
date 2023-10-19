da45c2d0e4780bfabbb343e21e3092c06adaeb1d
iExploze <54289660+iExploze@users.noreply.github.com>
2023-10-19 14:52:54 -0400

    stuff

--- src/use_case/clear_users/ClearOutputData.java
+++ src/use_case/clear_users/ClearOutputData.java
@@ -1,6 +1,15 @@
 package use_case.clear_users;
 
-// TODO Complete me
+import java.util.List;
 
 public class ClearOutputData {
+    private final List<String> deletedUsers;
+
+    public ClearOutputData(List<String> deletedUsers) {
+        this.deletedUsers = deletedUsers;
+    }
+
+    public List<String> getDeletedUsers() {
+        return deletedUsers;
+    }
 }
