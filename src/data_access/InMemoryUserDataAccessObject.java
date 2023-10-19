cfb872dd08f56a1445e059bea452f8386a048658
iExploze <54289660+iExploze@users.noreply.github.com>
2023-10-19 14:49:05 -0400

    changes to daos

--- src/data_access/InMemoryUserDataAccessObject.java
+++ src/data_access/InMemoryUserDataAccessObject.java
@@ -1,12 +1,15 @@
 package data_access;
 
 import entity.User;
+import use_case.clear_users.ClearUserDataAccessInterface;
 import use_case.signup.SignupUserDataAccessInterface;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterface {
+public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterface, ClearUserDataAccessInterface {
 
     private final Map<String, User> users = new HashMap<>();
 
@@ -25,5 +28,15 @@ public class InMemoryUserDataAccessObject implements SignupUserDataAccessInterfa
     @Override
     public void save(User user) {
         users.put(user.getName(), user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public void clearAllUsers() {
+        users.clear();
     }
 }
